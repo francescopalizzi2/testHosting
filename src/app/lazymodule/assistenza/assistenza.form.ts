d407a0956556553fefcd14923664f27b52064fce
StefanoDonati <crown.donas@gmail.com>
2021-11-19 12:52:39 +0100

    aggiunto footer in app.component.html

---
+++ src/app/lazymodule/assistenza/assistenza.form.ts
@@ -0,0 +1,11 @@
+export class FormAssistenza {
+
+    constructor(
+        public id : number,
+        public nome: string,
+        public oggetto: string,
+        public email: string,
+        public issue: string
+    ){}
+
+}
